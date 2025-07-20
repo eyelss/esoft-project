6a1e1e3b4a23d591c5955bd7d38eead34fcdc5d8
eyelss <68317860+eyelss@users.noreply.github.com>
2025-07-20 02:25:33 +0000

    node env echo

--- server/src/app.ts
+++ server/src/app.ts
@@ -25,6 +25,7 @@ const corsOptions = {
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
+console.log(process.env.NODE_ENV);
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
